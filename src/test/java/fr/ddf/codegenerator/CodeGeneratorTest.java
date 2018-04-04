345cd1bcc5dd42d56870944bcb740449697895fb
Alexander KIRILOV <alexander.kirilov@devoteam.com>
2018-04-04 17:40:09 +0200

    Added solution for step 1

---
+++ src/test/java/fr/ddf/codegenerator/CodeGeneratorTest.java
@@ -0,0 +1,47 @@
+package fr.ddf.codegenerator;
+
+import com.pholser.junit.quickcheck.Property;
+import com.pholser.junit.quickcheck.runner.JUnitQuickcheck;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(JUnitQuickcheck.class)
+public class CodeGeneratorTest {
+    private static Set<Integer> alreadyGeneratedCodes = new HashSet<>();
+    private static CodeGenerator codeGenerator;
+
+    @BeforeClass
+    public static void onlyOnce() {
+        codeGenerator = new CodeGenerator();
+    }
+
+    @Test
+    public void should_generate_code_betwean_1_and_10(){
+        //given
+        CodeGenerator codeGenerator = new CodeGenerator();
+
+        //when
+        int code = codeGenerator.generateCode();
+
+        //then
+        assertThat(code).isBetween(1, 10);
+    }
+
+    @Property(trials = 10)
+    public void generated_codes_should_not_repeat(){
+
+        //when
+        int generatedCode = codeGenerator.generateCode();
+
+        //then
+        assertThat(alreadyGeneratedCodes).doesNotContain(generatedCode);
+
+        alreadyGeneratedCodes.add(generatedCode);
+    }
+}
